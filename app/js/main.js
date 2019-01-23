fb93725bf1497fe6d24325284e9791c7041c1238
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2019-01-23 15:42:40 +0200

    First started template

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+;
+(function($) {
+    "use strict";
+
+    // var originalBGplaypen = $(".viewport").css("background-color"),
+    //     x, y, xy, bgWebKit, bgMoz,
+    //     lightColor = "rgba(255,255,255,0.75)",
+    //     gradientSize = 100;
+    // $('.viewport').mousemove(function(e) {
+
+    //     x = e.pageX - this.offsetLeft;
+    //     y = e.pageY - this.offsetTop;
+    //     xy = x + " " + y;
+
+    //     bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", " + gradientSize + ", from(" + lightColor + "), to(rgba(255,255,255,0.0))), " + originalBGplaypen;
+    //     bgMoz = "-moz-radial-gradient(" + x + "px " + y + "px 45deg, circle, " + lightColor + " 0%, " + originalBGplaypen + " " + gradientSize + "px)";
+
+    //     $(this)
+    //         .css({ background: bgWebKit })
+    //         .css({ background: bgMoz });
+
+    // }).mouseleave(function() {
+    //     $(this).css({ background: originalBGplaypen });
+    // });
+
+})(jQuery);
